0f4e87a9dc724ce3960bf83f5e4af00f427620d4
Marius Franzén <marius.franzen1@gmail.com>
2020-03-10 17:39:50 +0100

    The channels are now properly displayed

---
+++ RealbridgeApptestPrism/Model/RelevantRadioModel.cs
@@ -0,0 +1,46 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RealbridgeApptestPrism.Model
+{
+    public class RelevantRadioModel
+    {
+    }
+
+    public class RadioInfo : BindableBase
+    {
+        private RadioChannel.Channels.Channel _channel;
+        private RadioMusic.Music.Playlist _playlist;
+
+        public RadioChannel.Channels.Channel Channel
+        {
+            get
+            {
+                Console.WriteLine("General Kenobi!");
+                return _channel;
+            }
+            set
+            {
+                Console.WriteLine("!!!General Kenobi!");
+                SetProperty(ref _channel, value);
+            }
+        }
+
+        public RadioMusic.Music.Playlist Playlist
+        {
+            get
+            {
+                Console.WriteLine("You are a bold one.");
+                return _playlist;
+            }
+            set
+            {
+                Console.WriteLine("!!!You are a bold one.");
+                SetProperty(ref _playlist, value);
+            }
+        }
+
+    }
+}
